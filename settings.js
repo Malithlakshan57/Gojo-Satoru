1551caa0080472d611ac623bf9219260dd50c91b
Malithlakshan57 <106827459+Malithlakshan57@users.noreply.github.com>
2022-06-04 14:07:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94713441793']
+global.premium = ['94713441793']
+global.ownernomer = '94713441793'
+global.ownername = '🐦Lakiya boy'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
